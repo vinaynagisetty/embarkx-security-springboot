5007b644535dcb1c995a008f8a174a59cf19e5b0
vinay <vinaynagisetty7@gmail.com>
2025-05-15 12:24:57 +0530

    validste the api based on the credentails and roles from database

--- src/main/java/com/example/spring/security/SecurityConfig.java
+++ src/main/java/com/example/spring/security/SecurityConfig.java
@@ -1,27 +1,41 @@
 package com.example.spring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+            requests
+                    .requestMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated();
         });
         http.sessionManagement((session) -> {
             session.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS);
@@ -29,6 +43,12 @@ public class SecurityConfig {
 
 //        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.headers((headers) -> {
+            headers.frameOptions(
+                    HeadersConfigurer.FrameOptionsConfig::sameOrigin // Allow H2 console to be loaded in a frame from the same origin
+            );
+        });
+        http.csrf(AbstractHttpConfigurer::disable);
         return (SecurityFilterChain)http.build();
     }
     @Bean
@@ -42,5 +62,13 @@ public class SecurityConfig {
                 .password("{noop}user1")
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(admin, user);}
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
+        jdbcUserDetailsManager.createUser(user);
+
+        return jdbcUserDetailsManager;
+
+    }
+
 }
